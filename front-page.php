14e8452a42030f81e9b75a0eae8736609ea9986c
Angel Angeles <angelangelesiii@gmail.com>
2018-03-07 14:20:31 +0800

    Added new placeholders for front-page

--- front-page.php
+++ front-page.php
@@ -8,10 +8,30 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<section class="full-section" style="background-color: #939393; ">
-				<br>
-				<br>
-				<h1>This is the front page</h1>
+			<section class="full-section hero-banner" style="background-image: url(<?php echo get_template_directory_uri().'/dist/images/background/placeholder.jpg' ?>);">
+				<div class="overlay"></div>
+				<div class="hero-container">
+					<div class="wrapper">
+						<h2 class="hero-text">
+							We are Cornerstone.
+						</h2>
+						<p class="hero-subtext">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+					</div>
+				</div>
+			</section>
+
+			<section class="core">
+				<div class="wrapper-medium">
+					<div class="row">
+						<div class="core-item column medium-4 small-12">
+							<div class="image-container">
+								<div class="image"></div>
+							</div>
+						</div>
+						<div class="core-item column medium-4 small-12"></div>
+						<div class="core-item column medium-4 small-12"></div>
+					</div>
+				</div>
 			</section>
 
 		</main><!-- #main -->
