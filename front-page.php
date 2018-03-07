9f66c1fb4108f334719da493a8a75924b58a4719
Angel Angeles <angelangelesiii@gmail.com>
2018-03-07 11:24:29 +0800

    Changed front-page placeholder

--- front-page.php
+++ front-page.php
@@ -8,10 +8,11 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<div class="wrapper sample-wrap">
-			<h2>This is the Front Page</h2>
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci harum ea nam. Eaque error in exercitationem beatae nemo soluta amet. Quisquam necessitatibus quidem laboriosam perspiciatis veritatis facere eius mollitia voluptate!</p>
-		</div>
+			<section class="full-section" style="background-color: #939393; ">
+				<br>
+				<br>
+				<h1>This is the front page</h1>
+			</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
