58231d067dca079e301ef1468e3536ddfb407029
Angel Angeles <angelangelesiii@gmail.com>
2018-03-10 16:53:20 +0800

    Updated GSAP dependencies

--- functions.php
+++ functions.php
@@ -191,8 +191,9 @@ function cornerstone_main_scripts() {
     wp_enqueue_script( 'jquery' );
 
     // GSAP
-	wp_enqueue_script( 'GSAP', 'http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js', false, false, true);
-
+	wp_enqueue_script( 'GSAP', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js', false, false, true);
+	wp_enqueue_script( 'GSAP-scroll', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/plugins/ScrollToPlugin.min.js', false, false, true);
+	
 	// ScrollMagic
 	wp_enqueue_script( 'scrollmagic-main', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js', false, false, true);
 	wp_enqueue_script( 'scrollmagic-gsap', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js', false, false, true);
