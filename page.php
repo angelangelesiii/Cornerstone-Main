4e930d7671563f0636a3aa7c05676a978096b598
Angel Angeles <angelangelesiii@gmail.com>
2018-03-15 16:31:47 +0800

    Formatted "page"

--- page.php
+++ page.php
@@ -17,18 +17,23 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
+			<div class="wrapper-medium">
+				<?php
+				while ( have_posts() ) : the_post(); ?>
+	
+				<article class="article page-article overlapping">
+					<div class="article-title-container">
+						<h1 class="article-title"><?php the_title(); ?></h1>
+					</div>
+					<div class="article-content">
+						<?php the_content(); ?>
+					</div>
+				</article>
+					
+				<?php
+				endwhile; // End of the loop.
+				?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
