260ee56477c54f54a38fbe2f9bcde86bf098bdd2
Angel Angeles <angelangelesiii@gmail.com>
2018-03-22 11:00:33 +0800

    Moved contents of page.php to template part

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,39 +9,13 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-	<div class="entry-content">
-		<?php
-			the_content();
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'cornerstone-main' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						wp_kses(
-							/* translators: %s: Name of current post. Only visible to screen readers */
-							__( 'Edit <span class="screen-reader-text">%s</span>', 'cornerstone-main' ),
-							array(
-								'span' => array(
-									'class' => array(),
-								),
-							)
-						),
-						get_the_title()
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+<div class="wrapper-medium">
+	<article class="article page-article overlapping">
+		<div class="article-title-container">
+			<h1 class="article-title"><?php the_title(); ?></h1>
+		</div>
+		<div class="article-content">
+			<?php the_content(); ?>
+		</div>
+	</article>
+</div>
