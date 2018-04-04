efdffcdae3ab3e842a648f867a64383f0409b814
Angel Angeles <angelangelesiii@gmail.com>
2018-04-04 10:08:11 +0800

    Removed redundant wrapper from inside of page (post type)

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,13 +9,11 @@
 
 ?>
 
-<div class="wrapper-medium">
-	<article class="article page-article overlapping">
-		<div class="article-title-container">
-			<h1 class="article-title"><?php the_title(); ?></h1>
-		</div>
-		<div class="article-content">
-			<?php the_content(); ?>
-		</div>
-	</article>
-</div>
+<article class="article page-article overlapping">
+	<div class="article-title-container">
+		<h1 class="article-title"><?php the_title(); ?></h1>
+	</div>
+	<div class="article-content">
+		<?php the_content(); ?>
+	</div>
+</article>
