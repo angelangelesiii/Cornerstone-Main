bae5142e6f32be5f9600babad9eee1945e8388bc
Angel Angeles <angelangelesiii@gmail.com>
2018-05-03 10:38:18 +0800

    Footer first major design update

--- footer.php
+++ footer.php
@@ -15,7 +15,24 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="wrapper-medium">
-			<p>Ths is the footer</p>
+			<h2>Follow us on social media</h2>
+			<ul class="social-links">
+				<li class="social-link"><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+				<li class="social-link"><a href="#"><i class="fab fa-twitter"></i></a></li>
+				<li class="social-link"><a href="#"><i class="fab fa-instagram"></i></a></li>
+				<li class="social-link"><a href="#"><i class="fab fa-youtube"></i></a></li>
+			</ul>
+		</div>
+		<div class="wrapper-medium">
+			<div class="details">
+				<h2>Business Office Address</h2>
+				<p>2nd Level Robinsons Novaliches, Novaliches Quezon City</p>
+				<h2>Contact Numbers</h2>
+				<p>+63-2-332-8743  |  +63-905-501-0165</p>
+			</div>
+			<div class="footer-info">
+				<p>&copy; <?php echo date('Y'); ?> Cornerstone Global Inc.</p>
+			</div>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
