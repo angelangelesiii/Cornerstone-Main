da095d03ac5e70bbaa568e692417d7e05e932b61
Angel Angeles <angelangelesiii@gmail.com>
2018-05-03 10:40:36 +0800

    Initial index (blog page) formatting

--- index.php
+++ index.php
@@ -4,29 +4,46 @@
  */
 
 get_header(); ?>
+<div class="spacer"></div>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
-
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-		?>
-
-			<h1>blah</h1>
+			<div class="wrapper-big row collapse">
+				<div class="column large-9 small-12 main-container">
+					<?php if ( have_posts() ) : ?>
+	
+					<div class="blog-container">
+	
+					<?php
+						/* Start the Loop */
+						while ( have_posts() ) : the_post();
+					?>
 			
-		<?php
-			endwhile;
+						<article class="blog-item">
+							<div class="content">
+								<h1><?php the_title(); ?></h1>
+								<?php the_excerpt(); ?>
+							</div>
+						</article>
+						
+					<?php endwhile; ?>
+	
+					</div>
+	
+					<?php the_posts_navigation();
+					endif; ?>
+				</div>
+			
+				<div class="column large-3 small-12 sidebar-container">
+					<?php get_sidebar(); ?>
+				</div>
 
-			the_posts_navigation();
-
-		endif; ?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
