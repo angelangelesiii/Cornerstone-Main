b62321e7a9b2d68365c42aebf994da7dfcff5eb5
Angel Angeles <angelangelesiii@gmail.com>
2018-05-10 11:35:28 +0800

    Added horizontal scrolling to mouse wheel scroll event for main menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -143,5 +143,22 @@ jQuery(document).ready(function($){ // Document Ready
         });
     });
     
+    jQuery(function ($) {
+        $.fn.hScroll = function (amount) {
+            amount = amount || 120;
+            $(this).bind("DOMMouseScroll mousewheel", function (event) {
+                var oEvent = event.originalEvent, 
+                    direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta, 
+                    position = $(this).scrollLeft();
+                position += direction > 0 ? -amount : amount;
+                $(this).scrollLeft(position);
+                event.preventDefault();
+            })
+        };
+    });
+
+    $(document).ready(function(){
+        $('.header-menu-container').hScroll(40); // You can pass (optionally) scrolling amount
+    });
 
 });
