35d9f35048d4d5a5cf0febf2afcfbcb5b6afcb53
Angel Angeles <angelangelesiii@gmail.com>
2018-06-01 10:31:55 +0800

    Changed footer design and added menu

--- footer.php
+++ footer.php
@@ -24,24 +24,34 @@
 				?>
 			</div>
 		</nav>
-		<div class="wrapper-medium">
-			<h2>Follow us on social media</h2>
-			<ul class="social-links">
-				<li class="social-link"><a href="#"><i class="fab fa-facebook-square"></i></a></li>
-				<li class="social-link"><a href="#"><i class="fab fa-twitter"></i></a></li>
-				<li class="social-link"><a href="#"><i class="fab fa-instagram"></i></a></li>
-				<li class="social-link"><a href="#"><i class="fab fa-youtube"></i></a></li>
-			</ul>
+		<div class="wrapper-medium row footer-main">
+			<div class="column large-4 medium-6 small-12">
+				<h2>Follow us on social media</h2>
+				<ul class="social-links">
+					<li class="social-link"><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+					<li class="social-link"><a href="#"><i class="fab fa-twitter"></i></a></li>
+					<li class="social-link"><a href="#"><i class="fab fa-instagram"></i></a></li>
+					<li class="social-link"><a href="#"><i class="fab fa-youtube"></i></a></li>
+				</ul>
+			</div>
+			<div class="column large-4 medium-6 small-12">
+				<div class="details">
+					<h2>Business Office Address</h2>
+					<p>2nd Level Robinsons Novaliches, Novaliches Quezon City</p>
+				</div>
+			</div>
+			<div class="column large-4 medium-6 small-12">
+				<div class="details">
+					<h2>Contact Numbers</h2>
+					<p>+63-2-332-8743  |  +63-905-501-0165</p>
+				</div>
+			</div>
 		</div>
 		<div class="wrapper-medium">
-			<div class="details">
-				<h2>Business Office Address</h2>
-				<p>2nd Level Robinsons Novaliches, Novaliches Quezon City</p>
-				<h2>Contact Numbers</h2>
-				<p>+63-2-332-8743  |  +63-905-501-0165</p>
-			</div>
 			<div class="footer-info">
-				<p>&copy; <?php echo date('Y'); ?> Cornerstone Global Inc.</p>
+				<div class="divider"></div>
+				<img src="<?php echo get_template_directory_uri().'/dist/images/logo/cs_white.png' ?>" alt="Cornerstone" class="footer-logo">
+				<p class="info">&copy; <?php echo date('Y'); ?> Cornerstone Global Inc. All rights reserved.</p>
 			</div>
 		</div>
 	</footer><!-- #colophon -->
