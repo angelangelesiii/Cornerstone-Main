4cc05209064523acfc279f750351ab60a764ac05
Angel Angeles <angelangelesiii@gmail.com>
2018-07-19 07:59:11 +0800

    Removed outreach part and replaced it with custom elementor designable section

--- front-page.php
+++ front-page.php
@@ -6,14 +6,20 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main front-page">
 
 			<div class="transition-mark"></div>
 
 			<?php // CONTENT--------------------
 
 			// OUTREACH MESSAGE
-			get_template_part( 'fp-parts/fp-outreach');
+			// get_template_part( 'fp-parts/fp-outreach');
+			while(have_posts()): the_post(); ?>
+			<section class="main-content">
+				<?php the_content(); ?>
+			</section>
+			<?php
+			endwhile;
 
 			// CORNERSTONE INTRO
 			get_template_part( 'fp-parts/fp-cs-video');
