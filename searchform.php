db5ae9fba3f8cf1e793d01954674ec881317203b
Angel Angeles <angelangelesiii@gmail.com>
2018-07-19 09:10:32 +0800

    Added custom search form

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<!-- SEARCH FORM -->
+<form action="/" method="GET" class="search-form">
+	<input type="search" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="Search something..."/>
+	<button type="submit" id="submit"><i class="fas fa-search"></i></button>
+</form>
